ef3c93a7f38fd1a4f7828668a81e5c4340fbb411
Marketa <markettka@email.cz>
2019-09-08 11:10:52 +0200

    add canDeactivate guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRoutingModule } from './app-routing.module';
 import { AuthGuard } from './auth-guard.service';
 import { AuthSService } from './auth.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate.service';
 
 
 
@@ -35,7 +36,7 @@ import { AuthSService } from './auth.service';
     // HttpModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthGuard, AuthSService],
+  providers: [ServersService, AuthGuard, AuthSService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
